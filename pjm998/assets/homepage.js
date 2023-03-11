eb9533e277dbd07079f49dba4ebda1a7f64b6206
時無_ShiWu <756423078@qq.com>
2023-03-11 17:10:34 +0800

    modify navigator icon animate

--- pjm998/assets/homepage.js
+++ pjm998/assets/homepage.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     // 鼠标悬停
     $("#homepic").mouseover(function(){
-        // $("#homepic").attr('src','./assets/download_blob.png')
-        $("#homepic").animate({opacity:'0'},0,function(){
-            $("#homepic").attr('src','./assets/download_blob.png')
+        $("#homepic").stop(true,true)
+        $("#homepic").animate({opacity:'0'},200,function(){
+            $("#homepic").attr('src','./assets/home_active.png')
         })
-        $("#homepic").animate({opacity:'1'},1000)
+        $("#homepic").animate({opacity:'1'},900)
     });
     $("#homepic").mouseout(function(){
-        $("#homepic").animate({opacity:'0'},0,function(){
-            $("#homepic").attr('src','./assets/download.png')
+        $("#homepic").stop(true,true)
+        $("#homepic").animate({opacity:'0'},200,function(){
+            $("#homepic").attr('src','./assets/home.png')
         })
-        $("#homepic").animate({opacity:'1'},1000)
-        // $("#homepic").animate({opacity:'0'},0)
-        // $("#homepic").animate({opacity:'1'},1500)
+        $("#homepic").animate({opacity:'1'},900)
     });
 });
