4eb2b0ca637735fb2a546075e51c6192fd3bd787
時無_ShiWu <756423078@qq.com>
2023-03-12 11:04:58 +0800

    clean useless files

--- assets/jp/images/pc/3rd_anniv/kv/a.bat
+++
@@ -1,5 +0,0 @@
-echo off
-cd /d %~dp0
-wget -np --no-check-certificate https://dragalialost.com/assets/jp/images/pc/3rd_anniv/kv/05_dragalia_lost.png
-wget -np --no-check-certificate https://dragalialost.com/assets/jp/images/pc/3rd_anniv/kv/catch_text.png
-pause *.*
