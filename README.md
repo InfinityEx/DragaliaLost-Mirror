b6554c944a6ce6807c6b707cb6fe9f7d8cc0d537
時無_ShiWu <756423078@qq.com>
2023-03-17 22:12:39 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# DragaliaHP
-Dragalia HomePage backup
+# 失落的龙约-镜像站
 
-The Dragalia Lost has been close the gameserver in Nov.29, 2022 at 10:00 p.m. (PT), but I want to save sth, for unforgettable memories.**At least the official website ever existed.**
+# DragaliaLost-Mirror
 
+#### **纪念已经关服的龙约**
+
+失落的龙约已于2022年10月30日下午2点(GMT +8)正式关闭服务器并清空服务器所有资料，并于2023年3月1日下午2点(GMT +8)正式关闭漫画站及相关特设页面，但我个人想保存一些官网的东西，至少留下一些难以忘却的记忆，**最少证明官网曾经存在过。**
+The Dragalia Lost has been close the gameserver in Nov.30th 2022 at 6:00 a.m. (UTC) and close DragaliaLost Comic pages in Mar.1st 2023 at 6:00 a.m(UTC) but I want to save sth, for unforgettable memories.**At least the official website ever existed.**
+
+由于中国大陆对于网站的管理要求，镜像站的域名仍在备案中，具体镜像站网站运行情况将同步更新到仓库index.html中，也可访问Github Pages直接在线查看，相关已知的网站解析细节(以json数据居多)会以markdown文档形式更新到doc目录中。
+Because of website management requirements of China Mainland, the domain name of mirror site is still on registration. Mirror site status will update in index.html of this repository, you can browse Github Pages too. Relevant known details(more about json data) will be update to /doc directory as markdown document.
+
+目前网站仍有大量的页面没有完成复原，我本人也不是职业程序员出身，因此对于网站最终呈现的结果不做任何保证，如果您对镜像站有疑问或需要报告页面错误的可提交Issues。
+Currently, there are still a large number of pages on the website that have not been restored, and I am not a programmer by professional background. Therefore, I do not make any guarantee about the final presentation results of the website. If you have any questions about the mirror site or need to report page errors, you can submit Issues.
--- pjm998/assets/download.js
+++ pjm998/assets/download.js
@@ -1,5 +1,87 @@
 // Blob Download
+// function bdl(){
+//     $.ajax({
+//         url:"https://ghproxy.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v4.12.3/MAA-v4.12.3-win-arm64.zip",
+//         type:'GET',
+//         dataType:'blob',
+//         CORS:true,
+//         contentType:'application/x-www-form-urlencoded',
+//         secure:true,
+//         headers:{
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//     beforeSend:function(xhr){
+//         xhr.setRequestHeader("Authorization", "Basic " + btoa(""))
+//     },
+//     success:function(data){
+//         let dlink=window.URL.createObjectURL(data)
+//         const a=document.createElement('a')
+//         a.href=dlink
+//         a.download='demo.zip'
+//         document.body.appendChild(a)
+//         a.click()
+//         document.body.removeChild(a)
+//     }
+//     })
+// }
 function bdl(){
+    // 请求获取release地址
+    let jhr=new XMLHttpRequest();
+    // let rurl="https://gitee.com/api/v5/repos/cxasm/notepad--/releases/latest"
+    let rurl="https://api.github.com/repos/MaaAssistantArknights/MaaAssistantArknights/releases/latest"
+    jhr.open('GET',rurl,true);
+    jhr.send()
+    let ajson
+    jhr.onreadystatechange=function(){
+        if(jhr.readyState==4 && jhr.status===200){
+            ajson=jhr.responseText
+            let obj=JSON.parse(ajson)
+            // let url=obj["assets"][0]["browser_download_url"]
+            let url="https://ghproxy.com/"+obj["assets"][3]["browser_download_url"]
+            let fname=obj["assets"][3]["name"]
+            // let url='./mmd.zip'
+            console.log(url)
+            //`${url}?response-content-type=application/octet-stream`
+            let xhr=new XMLHttpRequest()
+            xhr.open('POST',url,true)
+            xhr.responseType="blob"
+            xhr.withCredentials=true
+            // xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+            xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+            // xhr.setRequestHeader("Access-Control-Allow-Origin","http://dl.com")
+            xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+            xhr.onprogress=function(e){
+                let percent=Math.floor(e.loaded/e.total*100)
+                document.getElementsByClassName("btn btn-secondary").item("a").innerText=`下载中:${percent}%`
+            }
+            
+            xhr.onload=function(e){
+                console.log(xhr.response)
+                let dllink=window.URL.createObjectURL(xhr.response)
+                let downloadlink=document.createElement('a')
+                downloadlink.download=fname
+                downloadlink.href=URL.createObjectURL(dllink)
+                downloadlink.click()
+            }
+            xhr.send()
+            // xhr.responseType="arraybuffer"
+            // xhr.onreadystatechange=event=>{
+            //     if(xhr.readyState==4 && xhr.status==200){
+            //         const fileName='fileName'
+            //         let blob=new Blob([xhr.response],{type:'application/octet-stream'})
+            //         // downloadlink=document.getElementsByClassName("btn btn-secondary").item("a")
+            //         let downloadlink=document.createElement('a')
+            //         downloadlink.download=fileName
+            //         downloadlink.href=URL.createObjectURL(blob)
+            //         downloadlink.click()
+            //     }
+            // }
+            
+        }
+    }
+}
+function bd2l(){
     // get json data
     let jhr=new XMLHttpRequest();
     // let rurl="https://gitee.com/api/v5/repos/cxasm/notepad--/releases/latest"
@@ -18,13 +100,13 @@ function bdl(){
             // let url=obj["assets"][0]["browser_download_url"]
             let url=obj["assets"][3]["browser_download_url"]
             let fname=obj["assets"][3]["name"]
-            getData(url,fname)
-            // jhr.send()
+            getData(fname)
+            jhr.send()
         }
     }
 }
-function getData(url,fname){
-    const durl=url+'?response-content-type=application/octet-stream'
+function getData(fname){
+    const durl="https://ghproxy.com/https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v4.12.3/MAA-v4.12.3-win-arm64.zip"
     const name=fname
     const a=document.createElement('a')
     a.href=durl
