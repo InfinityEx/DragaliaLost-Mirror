dd17d5d3a85a12e8d1be9d618c5c1b770ebb7fc3
InfinityEx <756423078@qq.com>
2023-05-29 14:38:31 +0800

    update chs layout

--- chs/index.php
+++ chs/index.php
@@ -2,6 +2,11 @@
     include '../layout/chs/hp.php';
 
     $dl_chs_header='dl_header';
+    $dl_chs_body='dl_body';
+    $dl_chs_footer='dl_footer';
+
     //show headers
     $dl_chs_header();
+    $dl_chs_body();
+    $dl_chs_footer();
 ?>
--- layout/chs/hp.php
+++ layout/chs/hp.php
@@ -8,4 +8,22 @@ function dl_header()
     include 'hp_script_a.html';
 }
 
+// body部分
+function dl_body()
+{
+    include 'hp_body_noscript.html';
+    include 'hp_body_languages.html';
+    include 'hp_body_modal.html';
+    include 'hp_body_first-view.html';
+    include 'hp_body_slider.html';
+    include 'hp_body_menu.html';
+    include 'hp_body_contents.html';
+    include 'hp_body_scripts.html';
+}
+
+function dl_footer()
+{
+    include 'hp_footer_main.html';
+    include 'hp_footer_extra.html';
+}
 ?>
