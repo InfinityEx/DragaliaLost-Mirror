d667aa599ef6d2e97aa086263134f577aa0b3c16
時無_ShiWu <756423078@qq.com>
2023-06-22 11:29:08 +0800

    update chs/world/story/kingdom

---
+++ template/chs/kingdom_head_meta.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=375, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="王道动作RPG手游　 Dragalia Lost ～失落的龙约～ 已于iOS、Googleplay正式上线！">
+    <meta name="keywords" content="Dragalia Lost,失落的龙约,官方网站,Nintendo,手机游戏">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://dragalialost.com/chs/">
+    <meta property="og:site_name" content="Dragalia Lost">
+    <meta property="og:title" content="阿尔贝里亚王国 王室 | Dragalia Lost ～失落的龙约～ 官方网站 | Nintendo">
+    <meta property="og:description" content="王道动作RPG手游　 Dragalia Lost ～失落的龙约～ 已于iOS、Googleplay正式上线！">
+    <meta property="og:image" content="https://dragalialost.com/assets/chs/images/og_image_dl.png">
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:image" content="https://dragalialost.com/assets/chs/images/og_image_dl.png">
+    <meta property="twitter:title" content="阿尔贝里亚王国 王室 | Dragalia Lost ～失落的龙约～ 官方网站 | Nintendo">
+    <meta property="twitter:description" content="王道动作RPG手游　 Dragalia Lost ～失落的龙约～ 已于iOS、Googleplay正式上线！">
+
+    <link rel="icon" type="image/png" href="/assets/common/favicon.png" sizes="32x32">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC:700&display=swap" rel="stylesheet">
